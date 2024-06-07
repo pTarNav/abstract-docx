8b12c9ab2b4474eda9cce6796a77bdd66eef1b32
Pau Tarragó Navarra <68395147+pTarNav@users.noreply.github.com>
2024-06-07 15:57:13 +0200

    implemented recursive OoxmlPackage parsing

--- ooxmldocx/ooxml_docx.py
+++ ooxmldocx/ooxml_docx.py
@@ -25,8 +25,7 @@ class OoxmlPart:
 		self.name, self.extension = self._get_name_and_extension_from_docx_file_name(
 			docx_file_content_element["name_tail"]
 		)
-		self.element = etree.fromstring(docx_file_content_element["content"].encode("utf-8"))
-		print(self.element.tag)
+		self.element = etree.fromstring(docx_file_content_element["content"])
 
 	@staticmethod
 	def _get_name_and_extension_from_docx_file_name(name_tail: str) -> tuple[str, str]:
@@ -42,17 +41,70 @@ class OoxmlPart:
 
 class OoxmlPackage:
 	name: str
-	parts: dict[str, OoxmlPart]
-	packages: Optional[OoxmlPackage] = None
+	parts: dict[str, OoxmlPart] = {}
+	packages: Optional[dict[str, OoxmlPackage]] = None
 	_rels: Optional[OoxmlPackage] = None
 
-	def __init__(self, docx_file_content: list) -> None:
+	def __init__(self, name: str, docx_file_content: list) -> None:
 		"""
 
 		:param docx_file_content:
 		"""
 
-		pass
+		self.name = name
+		self._load_package_content(docx_file_content=docx_file_content)
+
+	def _load_package_content(self, docx_file_content: list) -> None:
+		"""
+
+		:param docx_file_content:
+		"""
+
+		package_content = self._prepare_package_content(docx_file_content=docx_file_content)
+		self._process_package_content(package_content=package_content)
+
+	@staticmethod
+	def _prepare_package_content(docx_file_content: list) -> dict:
+		"""
+
+		:param docx_file_content:
+		:return package_content:
+		"""
+
+		package_content = {}
+		for docx_file_content_element in docx_file_content:
+			# Divide the file name by the first '/' character
+			f_name_head, f_name_tail = (docx_file_content_element["name_tail"].split("/", maxsplit=1) + [""])[:2]
+			package_content_element = {
+				"name_tail": f_name_tail if f_name_tail != "" else f_name_head,
+				"content": docx_file_content_element["content"]
+			}
+			if f_name_head in package_content.keys():
+				package_content[f_name_head].append(package_content_element)
+			else:
+				package_content[f_name_head] = [package_content_element]
+
+		return package_content
+
+	def _process_package_content(self, package_content: dict) -> None:
+		"""
+
+		:param package_content:
+		"""
+
+		for f_name, f_content in package_content.items():
+			if len(f_content) > 1:
+				# Process OoxmlPackage
+				match f_name:
+					case "_rels":
+						self._rels = OoxmlPackage(name=f_name, docx_file_content=f_content)
+					case _:
+						if self.packages is None:  # Initialize packages dict
+							self.packages = {}
+						self.packages[f_name] = OoxmlPackage(name=f_name, docx_file_content=f_content)
+			else:
+				# Process OoxmlPart
+				self.parts[f_name] = OoxmlPart(docx_file_content_element=f_content[0])
 
 
 class OoxmlDocx:
@@ -63,7 +115,7 @@ class OoxmlDocx:
 	custom_xml: Optional[OoxmlPackage] = None
 
 	_content_types: Optional[OoxmlPart] = None
-	_rels: OoxmlPackage
+	_rels: Optional[OoxmlPackage] = None
 
 	def __init__(self, docx_file_path: str) -> None:
 		"""
@@ -76,10 +128,19 @@ class OoxmlDocx:
 
 	def _load_docx_file_contents(self) -> None:
 		"""
-		Read the .docx file as a zip file in memory, loading each content into its correspondent ooxml docx content
+		Reads the .docx file as a zip file in memory, loading each content into its correspondent OoxmlDocx content
 		"""
 
-		docx_file_content_top = {}
+		docx_file_content = self._prepare_docx_file_content()
+		self._process_docx_file_content(docx_file_content=docx_file_content)
+
+	def _prepare_docx_file_content(self) -> dict:
+		"""
+
+		:return docx_file_content:
+		"""
+
+		docx_file_content = {}
 		with open(self.docx_file_path, "rb") as f:
 			with zipfile.ZipFile(BytesIO(f.read())) as zip_ref:
 				for f_name in zip_ref.namelist():
@@ -88,25 +149,32 @@ class OoxmlDocx:
 						f_name_head, f_name_tail = (f_name.split("/", maxsplit=1) + [""])[:2]
 						docx_file_content_element = {
 								"name_tail": f_name_tail if f_name_tail != "" else f_name_head,
-								"content": zip_ref.read(f_name).decode("utf-8")
+								"content": zip_ref.read(f_name)
 							}
-						if f_name_head in docx_file_content_top.keys():
-							docx_file_content_top[f_name_head].append(docx_file_content_element)
+						if f_name_head in docx_file_content.keys():
+							docx_file_content[f_name_head].append(docx_file_content_element)
 						else:
-							docx_file_content_top[f_name_head] = [docx_file_content_element]
+							docx_file_content[f_name_head] = [docx_file_content_element]
 
-		# Handle next steps outside the opened .docx file
-		for f_name, f_content in docx_file_content_top.items():
+		return docx_file_content
+
+	def _process_docx_file_content(self, docx_file_content: dict) -> None:
+		"""
+
+		:param docx_file_content:
+		"""
+
+		for f_name, f_content in docx_file_content.items():
 			match f_name:
 				case "word":
-					print(f_name, "0")
+					self.word = OoxmlPackage(name=f_name, docx_file_content=f_content)
 				case "docProps":
-					print(f_name, "1")
+					self.doc_props = OoxmlPackage(name=f_name, docx_file_content=f_content)
 				case "customXml":
-					print(f_name, "2")
+					self.custom_xml = OoxmlPackage(name=f_name, docx_file_content=f_content)
 				case "_rels":
 					print(f_name, "3")
 				case "[Content_Types].xml":
 					self._content_types = OoxmlPart(docx_file_content_element=f_content[0])
 				case _:
-					raise KeyError()
+					raise KeyError(f"Match not found for root .docx package/part {f_name}.")
