18a35c1ea6a20648aa2121170b8c03aa9a9f5db5
Pau Tarragó Navarra <68395147+pTarNav@users.noreply.github.com>
2024-06-19 09:16:18 +0200

    .

--- ooxmldocx/ooxml_graph.py
+++ ooxmldocx/ooxml_graph.py
@@ -1,7 +1,9 @@
+import re
+
 from lxml.etree import _Element as etreeElement
 
 from ooxmldocx.ooxml_docx import OoxmlDocx, OoxmlPackage, OoxmlPart
-
+from utils.html_server import HTMLServer
 
 def tab(d: int) -> str:
 	"""
@@ -17,20 +19,20 @@ class OoxmlGraph:
 		(
 			f"<!DOCTYPE html>\n"
 			f"<html>\n"
-			f"\t<head>\n\t\t<title>{file}</title>\n\t\t<style>{styles}</style>\n\t</head>"
+			f"\t<head>\n\t\t<title>{file}</title>\n\t\t<style>{styles}</style>\n\t</head>\n"
 			f"\t<body>\n{graph}\n\t</body>\n"
 			f"</html>"
 		)
 
 	html_ooxml_element_template: str = lambda _, depth, tag, children: \
 		(
-			f"{tab(d=depth)}<div class='ooxml_element_node'>\n"
-			f"{tab(d=depth+1)}<div class='ooxml_element_tag'>{tag}</div>\n"
+			f"{tab(d=2*depth)}<div class='ooxml_element_node'>\n"
+			f"{tab(d=2*depth+1)}<div class='ooxml_element_tag'>{tag}</div>\n"
 			# Children template (not included if leaf node)
-			f"{f'{tab(d=depth+1)}<div class={chr(39)}ooxml_element_children{chr(39)}{chr(10)}>' if len(children) != 0 else ''}"
+			f"{f'{tab(d=2*depth+1)}<div class={chr(39)}ooxml_element_children{chr(39)}>{chr(10)}' if len(children) != 0 else ''}"
 			f"{chr(10).join([child for child in children]) if len(children) != 0 else ''}"
-			f"{f'{chr(10)}{tab(d=depth+1)}</div>{chr(10)}' if len(children) != 0 else ''}"
-			f"{tab(d=depth)}</div>"
+			f"{f'{chr(10)}{tab(d=2*depth+1)}</div>{chr(10)}' if len(children) != 0 else ''}"
+			f"{tab(d=2*depth)}</div>"
 		)
 
 	def __init__(self, ooxml_structure: OoxmlDocx | OoxmlPackage | OoxmlPart):
@@ -64,8 +66,13 @@ class OoxmlGraph:
 		:return:
 		"""
 
+		styles = (
+			f":root {{ --indentation-size: 2rem; }}\n"
+			f".ooxml_element_node {{ padding-left: 2rem; }}"
+		)
+
 		self.html_output = self.html_main_template(
-			file="x", styles="x", graph=self._build_ooxml_element(element=self.ooxml_structure.element)
+			file="x", styles=styles, graph=self._build_ooxml_element(element=self.ooxml_structure.element)
 		)
 
 	def _build_ooxml_element(self, element: etreeElement, depth: int = 0) -> str:
@@ -78,6 +85,14 @@ class OoxmlGraph:
 
 		return self.html_ooxml_element_template(
 			depth=depth,
-			tag=element.tag,
-			children=[self._build_ooxml_element(element=child, depth=depth+2) for child in element]
+			tag=re.sub(r"\{.*?}(\w)", lambda m: m.group(0)[-1], element.tag),
+			children=[self._build_ooxml_element(element=child, depth=depth+1) for child in element]
 		)
+
+	def open_graph(self, port: int = 8000) -> None:
+		"""
+
+		"""
+
+		html_server = HTMLServer(html=self.html_output, port=port)
+		html_server.serve_html()
---
+++ utils/html_server.py
@@ -0,0 +1,83 @@
+import sys
+import threading
+import http.server
+import socketserver
+import webbrowser
+import requests
+
+
+class HTMLServer:
+	"""
+
+	"""
+
+	def __init__(self, html: str, port: int = 8000) -> None:
+		"""
+
+		:param html:
+		:param port:
+		"""
+		self.html = html
+		self.port = port
+		self.httpd = None
+		self.server_thread = None
+
+	class RequestHandler(http.server.SimpleHTTPRequestHandler):
+		def __init__(self, *args, html: str, **kwargs):
+			self.html = html
+			super().__init__(*args, **kwargs)
+
+		def do_GET(self) -> None:
+			self.send_response(200)
+			self.send_header("Content-type", "text/html")
+			self.end_headers()
+			self.wfile.write(self.html.encode('utf-8'))
+
+	def _serve_html(self) -> None:
+		"""
+
+		"""
+
+		#
+		def handler(*args, **kwargs):
+			return self.RequestHandler(*args, html=self.html, **kwargs)
+
+		self.httpd = socketserver.TCPServer(("", self.port), handler)
+
+		try:
+			print(f"Serving html at port {self.port}.")
+			self.httpd.serve_forever()
+		except Exception as e:
+			raise Exception(f"Server error: {e}")
+		finally:
+			self.httpd.server_close()
+
+	def _open_browser(self) -> None:
+		"""
+
+		"""
+
+		webbrowser.open(f"http://localhost:{self.port}")
+		input("Press any key to close the server...\n")
+
+	def serve_html(self) -> None:
+		"""
+
+		"""
+
+		# Start the server in a separate thread
+		self.server_thread = threading.Thread(target=self._serve_html)
+		self.server_thread.daemon = True
+		self.server_thread.start()
+
+		# Open the web browser to the local server and wait for it to close
+		try:
+			self._open_browser()
+		except KeyboardInterrupt:
+			print("Interrupted. Closing server.")
+		finally:
+			print("Shutting down server.")
+			self.httpd.shutdown()
+			self.httpd.server_close()
+			# Ensure the thread joins properly
+			self.server_thread.join()
