9de005eab5992125fc67fe3399c336c7ca835e3e
pTarNav <p.tarragonavarra@gmail.com>
2025-04-07 14:20:51 +0200

    deep copy for normalized ooxml docx, to be able to keep the original, and perform changes on the copied object

--- abstract_docx/main.py
+++ abstract_docx/main.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from typing import Optional
+import copy
 
 from utils.pydantic import ArbitraryBaseModel
 
@@ -11,12 +13,13 @@ class AbstractDocx(ArbitraryBaseModel):
 	"""
 	file_path: str
 	ooxml_docx: OoxmlDocx
+	normalized_ooxml_docx: OoxmlDocx  # Initialized as a deep copy of the inputted ooxml_docx
 
 	@classmethod
 	def read(cls, file_path: str) -> AbstractDocx:
 		ooxml_docx: OoxmlDocx = OoxmlDocx.read(file_path=file_path)
 
-		return cls(file_path=file_path, ooxml_docx=ooxml_docx)
+		return cls(file_path=file_path, ooxml_docx=ooxml_docx, normalized_ooxml_docx=copy.deepcopy(ooxml_docx))
 	
 	
 if __name__ == "__main__":
