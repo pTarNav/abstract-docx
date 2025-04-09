0b63a5feb39e594f0d4b5815e78a18ecb2617ae2
pTarNav <p.tarragonavarra@gmail.com>
2025-04-09 17:39:59 +0200

    refining format data structure

--- abstract_docx/normalization/styles_normalization.py
+++ abstract_docx/normalization/styles_normalization.py
@@ -1,14 +1,44 @@
 from __future__ import annotations
+from typing import Optional
 from ooxml_docx.structure.styles import OoxmlStyles
+from ooxml_docx.structure.properties import ParagraphProperties, RunProperties
+from ooxml_docx.ooxml import OoxmlElement
 
 from utils.pydantic import ArbitraryBaseModel
 
-from abstract_docx.views.format import Style
+from abstract_docx.views.format import Style, VisualProperties, Script, Justification, Indentation
 
+
+def load_default_style(ooxml_styles: OoxmlStyles) -> OoxmlStyles:
+	run_properties: Optional[RunProperties] = ooxml_styles.doc_defaults.default_run_properties
+	if run_properties is not None:
+		font_size: Optional[float] = run_properties.xpath_query(query="./w:sz/@w:val", singleton=True)
+		font_size = float(font_size) if font_size is not None else 1.0  # TODO: investigate further about default font size
+
+		# TODO: color
+
+		bold: bool = run_properties.xpath_query(query="./w:b", singleton=True) is not None
+		italic: bool = run_properties.xpath_query(query="./w:i", singleton=True) is not None
+		
+		# Even though it is not a toggle property, we will treat it as such
+		underline: bool = run_properties.xpath_query(query="./w:i", singleton=True) is not None
+		
+		script: Script = Script.from_ooxml_val(v=run_properties.xpath_query(query="./w:vertAlign/@w:val", singleton=True))
+	else:
+		font_size 
+
+
+	paragraph_properties: Optional[ParagraphProperties] = ooxml_styles.doc_defaults.default_paragraph_properties
+	if paragraph_properties is not None:
+		justification: Justification = Justification.from_ooxml_val(
+			v=paragraph_properties.xpath_query(query="./w:jc/@w:val", singleton=True)
+		)
+		indentation: Indentation = Indentation.from_ooxml(
+			element=paragraph_properties.xpath_query(query="./w:ind", singleton=True)
+		)
 
 
 
 def styles_normalization(ooxml_styles: OoxmlStyles) -> OoxmlStyles:
-	for style in ooxml_styles.roots.paragraph:
-		print(style.id)
+	default_style: Style = load_default_style(ooxml_styles=ooxml_styles)
 		
--- abstract_docx/views/format.py
+++ abstract_docx/views/format.py
@@ -6,33 +6,120 @@ from colour import Color
 
 from utils.pydantic import ArbitraryBaseModel
 
-
-class Indentation(ArbitraryBaseModel):
-	"""
-	|			|<--first-->aaaaa|		   |
-	|<--start-->|aaaaaaaaaaaaaaaa|<--end-->|
-	|			|aaaaaaaaaa.     |		   |
-	"""
-	start: float
-	end: float
-	first: float
-
-
-class Justification(Enum):
-	start = "START"				# |The quick brown fox.                |
-	center = "CENTER"			# |        The quick brown fox.        |
-	end = "END"					# |                The quick brown fox.|
-	both = "BOTH"				# |The      quick       brown      fox.|
-	distribute = "DISTRIBUTE"	# |T h e  q u i c k  b r o w n  f o x .|
+from ooxml_docx.ooxml import OoxmlElement
 
 
 class Script(Enum):
+	"""
+	Defaults to normal
+	"""
 	normal = None
 	superscript = "SUPERSCRIPT"
 	subscript = "SUBSCRIPT"
 
+	@classmethod
+	def default(cls) -> Script:
+		return cls.normal
 
-class VisualProperties(ArbitraryBaseModel):
+	@classmethod
+	def from_ooxml_val(cls, v: Optional[str]) -> Script:
+		"""
+		Convert the OOXML attribute value to the corresponding Script enum.
+		"""
+		return {
+			"baseline": cls.normal,
+			"superscript": cls.superscript,
+			"subscript": cls.subscript,
+		}.get(v, cls.default())
+
+
+class RunSpecificStyleProperties(ArbitraryBaseModel):
+	font_size: float
+	color: Color
+	bold: bool
+	italic: bool
+	underline: bool
+	script: Script
+
+	@classmethod
+	def default(cls) -> RunSpecificStyleProperties:
+		# TODO: investigate further about default font size
+		return cls(
+			font_size=1.0, color=Color(color="black"), bold=False, italic=False, underline=False, script=Script.default()
+		)
+
+
+class Indentation(ArbitraryBaseModel):
+	"""
+	|			|<--first-->Aaaaa|		   |
+	|<--start-->|aaaaaaaaaaaaaaaa|<--end-->|
+	|			|aaaaaaaaaa.     |		   |
+	"""
+	start: float = 0.0
+	end: float = 0.0
+	first: float = 0.0
+
+	@classmethod
+	def from_ooxml(cls, element: Optional[OoxmlElement]) -> Indentation:
+		"""
+		Convert the OOXML attribute value to the corresponding Justification enum.
+		"""
+		start: Optional[float] = element.xpath_query(query="./@w:start|./@w:left", singleton=True)
+		end: Optional[float] = element.xpath_query(query="./@w:end|./@w:right", singleton=True)
+		
+		first: Optional[float] = element.xpath_query(query="./@w:hanging", singleton=True)
+		if first is not None:
+			first: Optional[float] = element.xpath_query(query="./@w:firstLine", singleton=True)
+		else:
+			first = -float(first)
+
+		return cls(
+			start=float(start) if start is not None else 0.0,
+			end=float(end) if end is not None else 0.0,
+			first=float(first) if first is not None else 0.0
+		)
+
+
+class Justification(Enum):
+	"""
+	Defaults to start.
+	"""
+	start = "START"			# |The quick brown fox.                |
+	center = "CENTER"		# |        The quick brown fox.        |
+	end = "END"				# |                The quick brown fox.|
+	justified = "JUSTIFIED"	# |The      quick       brown      fox.|
+
+	@classmethod
+	def default(cls) -> Justification:
+		return cls.start
+
+	@classmethod
+	def from_ooxml_val(cls, v: Optional[str]) -> Justification:
+		"""
+		Convert the OOXML attribute value to the corresponding Justification enum.
+		"""
+		return {
+			"start": cls.start,
+			"left": cls.start,
+			"end": cls.end,
+			"right": cls.end,
+			"center": cls.center,
+			"both": cls.justified,
+			"distribute": cls.justified  # Even though it is supposed to be it's own type...
+		}.get(v, cls.default())
+
+class ParagraphSpecificStyleProperties(ArbitraryBaseModel):
+	justification: Justification
+	indentation: Indentation
+
+	@classmethod
+	def default(cls) -> RunSpecificStyleProperties:
+		return cls(justification=Justification.default(), indentation=Indentation())
+
+class TableSpecificStyleProperties(ArbitraryBaseModel):
+	todo: str
+
+class StyleProperties(ArbitraryBaseModel):
 	"""
 	The relevant formatting properties are:
 	 - font_size:
@@ -45,27 +132,24 @@ class VisualProperties(ArbitraryBaseModel):
 	 - indentation:
 	TODO: Expand properties
 	"""
+	run_properties: RunSpecificStyleProperties
+	paragraph_properties: ParagraphSpecificStyleProperties
 
-	font_size: int
-	color: Color
-	bold: bool
-	italic: bool
-	underline: bool # TODO: Complicate this with the types of underlines docx provides
-	script: Script
-	justification: Justification
-	indentation: Indentation
+	table_properties: Optional[TableSpecificStyleProperties] = None
 
 
 class Style(ArbitraryBaseModel):
 	"""
-	To simplify style management and processing, generalizes the concept of a style given to a paragraph,
-	Gathering all the relevant properties from the different OOXML style types that have an effect from a paragraph viewpoint,
+	To simplify style management and processing, generalizes the concept of a style given to a block,
+	Gathering all the relevant properties from the different OOXML style types that have an effect from a block viewpoint,
 	both at a paragraph and character level properties (<w:pPr> and <w:rPr>).
 	"""
 	id: str
 	
 	parent: Optional[Style] = None
 	children: Optional[list[Style]] = None
+
+	properties: StyleProperties
 
 
 class Numbering(ArbitraryBaseModel):
