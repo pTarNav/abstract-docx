be1424e51f4314943e2cb41fae81cf8cc173f228
pTarNav <p.tarragonavarra@gmail.com>
2025-06-14 17:27:00 +0200

    delete all the files because they have been moved to src, how i hate python way of structuring packages, nothing seems to have problems when testing internally and then whenever you try use it outside boom everything breaks

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="abstract_docx",
     version="0.1",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
