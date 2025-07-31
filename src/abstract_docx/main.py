8f927dfdcd8b5fa456e3f4c255690ab3f5a41c9d
pTarNav <p.tarragonavarra@gmail.com>
2025-07-31 09:41:05 +0200

    .

--- src/abstract_docx/main.py
+++ src/abstract_docx/main.py
@@ -201,7 +201,7 @@ class AbstractDocx(ArbitraryBaseModel):
 
 	
 if __name__ == "__main__":
-	test_files = ["SB004_report"]
-	x = AbstractDocx.read(file_path=f"test/unfccc/{test_files[3]}.docx")
+	test_files = ["SB004_report", "cma6_report"]
+	x = AbstractDocx.read(file_path=f"test/unfccc/{test_files[1]}.docx")
 	x()
 	x.print()
